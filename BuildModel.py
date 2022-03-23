a027294ad2d49cbcaad7623d320929c28674caaf
Timclewis <38727434+Timclewis@users.noreply.github.com>
2022-03-23 15:49:42 +0100

    Dataspell verion of pubid

---
+++ BuildModel.py
@@ -0,0 +1,47 @@
+import datetime
+import os
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.callbacks import LambdaCallback, TensorBoard
+from tensorflow.keras.models import Model, load_model
+
+from DataGenerator import DataGenerator
+
+
+class BuildModel:
+    def __init__(self, data_generator):
+        self.width = data_generator.width
+        self.height = data_generator.height
+        self.class_num = data_generator.class_num
+        self.test_num = data_generator.test_num
+        self.class_names = data_generator.class_names
+        self.steps = data_generator.steps
+        self.val_steps = data_generator.val_steps
+        self.batch_size = data_generator.batch_size
+        self.train_data = data_generator.train_data
+        self.valid_data = data_generator.valid_data
+        self.test_data = data_generator.test_data
+        self.test_labels = data_generator.test_labels
+        self.current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+
+        # self.publisher_names = list(self.train_data.class_indices.keys())[0:self.class_num]
+
+    def compile_model(self, epochs, network, pooling, optimizer, learn_rate, summary):
+        self.epochs = epochs
+        self.network = network
+        self.pool = pooling
+        self.opt = optimizer
+        self.lr = learn_rate
+
+        base = self.network(include_top=False, weights='imagenet',
+                            input_shape=(self.width, self.height, 3), pooling=self.pool)
+        opt = self.opt(learning_rate=self.lr)
+        x = base.output
+        # x = layers.Dense(512, activation= 'relu')(x)
+        x = layers.Dense(self.class_num, activation='softmax')(x)
+        self.model = Model(base.input, x)
+        self.model.compile(loss='categorical_crossentropy', metrics=['acc'], optimizer=opt)
+
+        if summary:
+            self.model.summary()
